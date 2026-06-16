ca0a830550e2e8fb57998673a1613a2a242f621d
tijanab44 <104036730+tijanab44@users.noreply.github.com>
2026-06-16 14:17:28 +0200

    Add files via upload

---
+++ javasc/javascript.js
@@ -0,0 +1,27 @@
+let firstName = "John";
+let lastName = "Doe";
+console.log(firstName);
+console.log(lastName);
+
+let age = 11;
+console.log(age); //outputs 11 to the console
+age = 35;
+console.log(age); //sad je output: 35
+// sve ukupno ce izbaciti - John \n Doe \n 11 \n 35 \n
+
+const pi = 3.14;
+// pi = 30; ovo bi dalo gresku
+console.log(pi); 
+
+console.log(23 + 97);
+console.log(4 + 6 + 9) / 77;
+let a = 10;
+console.log(a);
+a = 15;
+console.log(a);
+let b = (7 * a);
+let max = 57;
+let actual = max - 13;
+let percentage = actual / max;
+console.log(b, max, actual, percentage);
+
